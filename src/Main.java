1e5e45d3bbf9583cfd1c0d1c8d848c2bf38c3993
kcheung3102 <kcheung3102@gmail.com>
2019-10-21 16:51:54 -0400

    commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int myNum = 2;
+        System.out.println(myNum);
+        double myOtherNum = 1.7938;
+        System.out.println(myOtherNum);
+    }
+}
